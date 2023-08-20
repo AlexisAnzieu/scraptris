9da9bb1d694e9e1068051f49bfccd4d7cb5e6655
alexis anzieu <alexis.anzieu@gmail.com>
2023-08-20 19:40:43 -0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import { ClientComponent } from "./components/ClientComponent";
 import { Properties } from "./components/MapComponent";
 
 export default async function Home() {
-    const result: any = await fetch(
+    const response: Response = await fetch(
         "http://vps-936fc4fa.vps.ovh.net:3500/properties"
     );
 
-    const properties: Properties[] = await result.json();
+    const properties: Properties[] = await response.json();
 
     return <ClientComponent properties={properties} />;
 }
